8d26a871570027f58e523fd216e43b6c727d4342
Naren <76785483+batarang24@users.noreply.github.com>
2024-04-27 01:19:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,26 +1,13 @@
 const express=require('express')
 const http=require('http')
-const cors=require('cors')
+
 
 const app=express()
 const server=http.createServer(app)
-app.use(cors());
-const socketIo=require('socket.io')(server,{
-    cors:{
-        origin:'http://localhost:3000'
-    }
-})
 
-socketIo.on('connection',(s)=>{
-    
-    //console.log(s.id)
-    
-    s.on('send',(data)=>{
-        console.log(data)
-        s.broadcast.emit('receive',data.input)
-    })
-})
-server.listen(3001,()=>{
+const port=process.env.PORT || 3000;
+server.listen(port,()=>{
     console.log("server running in 3001")
+    res.send(process.env.PORT)
 })
 
